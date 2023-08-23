f71962b7834725556fe0d9682863f801c3058825
Anand Raj Singh <singhpariharanandraj@gmail.com>
2023-08-23 11:34:13 +0530

    Login Frontend

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -20,8 +20,7 @@ const NavBar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = "Anand Raj Singh";
-  // const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
